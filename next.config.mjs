21d453f2fc752d95388a9f5e097b727e989205bf
Ton Nom <ton.email@example.com>
2025-10-16 17:23:40 +0200

    fix(deploy): remove redirect to fix 404 error

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/dashboard',
-        permanent: true,
-      },
-    ]
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
